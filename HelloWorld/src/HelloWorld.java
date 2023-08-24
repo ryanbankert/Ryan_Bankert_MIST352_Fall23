4495bc05fbd59d94fb5e1008c58e9b29480d79ce
ryanbankert <rjb00014@mix.wvu.edu>
2023-08-24 12:40:39 -0400

    Added HelloWorld project to the repo

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ * @author Ryan Bankert
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to MIST352");
+	}
+
+}
