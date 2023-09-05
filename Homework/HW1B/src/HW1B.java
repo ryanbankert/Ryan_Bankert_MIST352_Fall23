e6b7e793e86beb43735ddebd4ef2130d976d4bad
ryanbankert <rjb00014@mix.wvu.edu>
2023-09-05 12:24:17 -0400

    Added HW1B

--- Homework/HW1B/src/HW1B.java
+++ Homework/HW1B/src/HW1B.java
@@ -4,16 +4,15 @@
 
 /**
  * 
- * @author Ryan Bankert
  */
-public class HelloWorld {
+public class HW1B {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to MIST352");
+
 	}
 
 }
