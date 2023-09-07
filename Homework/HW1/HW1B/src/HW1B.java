fff8297d8c49f3598c2052e5788db004c35f25d6
ryanbankert <rjb00014@mix.wvu.edu>
2023-09-07 11:42:25 -0400

    Submitted HW#1

---
+++ Homework/HW1/HW1B/src/HW1B.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ryan Bankert
+ * Class: MIST352-Fall23
+ * HW#1
+ */
+public class HW1B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1;
+		double num2;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter value for num1 " );
+		num1 = keyboard.nextInt();
+		
+		System.out.println("Enter value for num2: " );
+		num2 = keyboard.nextDouble();
+		
+		double sum = num1 * num2;
+		
+		System.out.println("The sum of the values are: " + sum);
+		
+		keyboard.close();
+	}
+
+}	
