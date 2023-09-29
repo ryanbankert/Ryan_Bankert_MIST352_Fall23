1825780d0bf5f425e848040e538ae64d8c13508d
ryanbankert <rjb00014@mix.wvu.edu>
2023-09-28 23:32:23 -0400

    Submitted to Github

--- Exams/Exam1B_ChatGPT/src/Exam1B_ChatGPT.java
+++ Exams/Exam1B_ChatGPT/src/Exam1B_ChatGPT.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 
+public class Exam1B_ChatGPT {
 
-	public class Exam1B_ChatGPT {
+    public static void main(String[] args) {
+        // Create a Scanner object for taking input
+        Scanner scanner = new Scanner(System.in);
 
-	    public static void main(String[] args) {
-	        // Create a Scanner object for taking input
-	        Scanner scanner = new Scanner(System.in);
+        // Display welcome message
+        System.out.println("Welcome!");
 
-	        // Display welcome message
-	        System.out.println("Welcome!");
+        // Ask user for the text input
+        System.out.print("Enter the string to work with: ");
+        String userInput = scanner.nextLine();
 
-	        // Ask user for the input string
-	        System.out.print("Enter the string to search within: ");
-	        String userInput = scanner.nextLine();
+        // Print out the length of the input
+        System.out.println("Length of the input is: " + userInput.length());
 
-	        
-	        // Says character instead of letter, terminology was different
-	          // Ask user for a character to find in the input
-	        System.out.print("Enter a character to find its first occurrence: ");
-	       
-	        char charToFind = scanner.nextLine().charAt(0);
+        // Ask user for a character to find in the input
+        System.out.print("Enter a character to find its first occurrence: ");
+        char charToFind = scanner.nextLine().charAt(0);
 
-	       // Their code starts at 0 and counts up the characters, which gives incorrect results
-	        // Find the location of the first occurrence of the character
-	        int location = userInput.indexOf(charToFind);
+        // Find the location of the first occurrence of the character
+        int location = userInput.indexOf(charToFind);
 
-	        // Print out the location of the first occurrence of that character
-	        System.out.println("The character '" + charToFind + "' first occurs at position " + location + ".");
+        // Print out the location of the first occurrence of that character
+        System.out.println("The character '" + charToFind + "' first occurs at position " + location + ".");
 
-	        // Close the scanner
-	        scanner.close();
-	    }
-	}
+        // Close the scanner
+        scanner.close();
+    }
+}
