fb363e292c7da7c70f29ef3ed4c7288b53eb1404
ryanbankert <rjb00014@mix.wvu.edu>
2023-09-28 22:47:09 -0400

    Submitted on GitHub

---
+++ Exams/Exam1B/src/Exam1B.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * @author Ryan Bankert
+ * Class: MIST352-Fall23
+ * Exam#1B
+ * 
+ * This code finds the amount of letters in an inputed message.
+ *
+ *
+ */
+public class Exam1B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message;
+		
+		int Stringsize;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		// Welcomes users
+		System.out.println("Welcome, please provide a message.");
+		// Saves the input from the user
+		message = keyboard.nextLine();
+		
+		// Finds the length of the message
+		Stringsize = message.length();
+		
+		// Displays the length of the message
+		System.out.println("The length of your sentence (with spaces) is: " + Stringsize);
+		
+		// Ask user for letter input
+		System.out.println("Enter a Letter that was in the message.");
+		char LetAT = keyboard.next().charAt(0);
+		
+		//Finds the letter in the message.
+		int LetPos = message.indexOf(LetAT) +1;
+		
+		// Outputs results
+		System.out.println("The first occurance of "+ LetAT + " is at the position of: " + LetPos + ".");
+		
+		//Closes the Scanner
+		keyboard.close();
+	}
+
+}
---
+++ Exams/Exam1B_ChatGPT/src/Exam1B_ChatGPT.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+	public class Exam1B_ChatGPT {
+
+	    public static void main(String[] args) {
+	        // Create a Scanner object for taking input
+	        Scanner scanner = new Scanner(System.in);
+
+	        // Display welcome message
+	        System.out.println("Welcome!");
+
+	        // Ask user for the input string
+	        System.out.print("Enter the string to search within: ");
+	        String userInput = scanner.nextLine();
+
+	        // Ask user for a character to find in the input
+	        System.out.print("Enter a character to find its first occurrence: ");
+	        char charToFind = scanner.nextLine().charAt(0);
+
+	        // Find the location of the first occurrence of the character
+	        int location = userInput.indexOf(charToFind);
+
+	        // Print out the location of the first occurrence of that character
+	        System.out.println("The character '" + charToFind + "' first occurs at position " + location + ".");
+
+	        // Close the scanner
+	        scanner.close();
+	    }
+	}
